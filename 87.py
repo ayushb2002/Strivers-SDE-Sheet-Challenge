640fe2ab19c891bcff5cce1522a76f2c17e26e5f
Ayush Bansal <ayushb6732@gmail.com>
2023-06-28 17:07:35 +0530

    Solved q87

--- 87.py
+++ 87.py
@@ -1,18 +1,13 @@
 def nextGreater(arr,n):
-    res = []
+    res = [-1]*n
     stack = []
     for i in range(n-1, -1, -1):
-        flag = 0
         while len(stack) > 0:
             if stack[0] > arr[i]:
-                res.insert(0, stack[0])
-                flag = 1
+                res[i] = stack[0]
                 break
             else:
                 stack.pop(0)
-            
-        if not flag:
-            res.insert(0, -1)
         
         stack.insert(0, arr[i])
     
