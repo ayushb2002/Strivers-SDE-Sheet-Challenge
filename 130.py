1762cbc4f340d055d82eea2f9cb2fedc7d18d0a3
Ayush Bansal <ayushb6732@gmail.com>
2023-07-11 12:52:02 +0530

    Solved q130

--- 130.py
+++ 130.py
@@ -1,25 +1,26 @@
-class   TreeNode :
-    def __init__(self, data) :
+class BinaryTreeNode:
+    def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
 
-def construct(start, end, postorder, pIndex, d):
- 
-    if start > end:
-        return None, pIndex
-    root = TreeNode(postorder[pIndex])
-    pIndex = pIndex - 1
-    index = d[root.data]
- 
-    root.right, pIndex = construct(index + 1, end, postorder, pIndex, d)
-    root.left, pIndex = construct(start, index - 1, postorder, pIndex, d)
-    return root, pIndex
-def getTreeFromPostorderAndInorder(postorder, inorder):
-    n = len(inorder)
-    d = {}
-    for i, e in enumerate(inorder):
-        d[e] = i
-    pIndex = n - 1
-    return construct(0, n - 1, postorder, pIndex, d)[0]
+
+def buildBinaryTree(preorder:list, inorder:list):
+    VAL_TO_INORDER_IDX = {inorder[i]: i for i in range(len(inorder))}
+    def buildTreePartition(preorder, inorder_start, inorder_end):
+        if not preorder or inorder_start < 0 or inorder_end > len(inorder):
+            return None
+
+        root_val = preorder[0]
+        root_inorder_idx = VAL_TO_INORDER_IDX[root_val]
+        if root_inorder_idx > inorder_end or root_inorder_idx < inorder_start:
+            return None
+
+        root = BinaryTreeNode(preorder.pop(0))
+        root.left = buildTreePartition(preorder, inorder_start, root_inorder_idx - 1)
+        root.right = buildTreePartition(preorder, root_inorder_idx + 1, inorder_end)
+
+        return root
+
+    return buildTreePartition(preorder, 0, len(inorder) - 1)
