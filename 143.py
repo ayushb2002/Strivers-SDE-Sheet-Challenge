70c4099c858a293b8cf19e4dca90609d172c84fe
Ayush Bansal <ayushb6732@gmail.com>
2023-07-16 21:44:38 +0530

    Solved q143

---
+++ 143.py
@@ -0,0 +1,13 @@
+def floorInBST(root, X):
+    ans = -1
+    while root:
+        if root.data == X:
+            ans = X
+            break
+        if root.data > X:
+            root = root.left if root.left else None
+        elif root.data < X:
+            ans = root.data
+            root = root.right if root.right else None
+        
+    return ans
